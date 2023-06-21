f6254442d9e78d31fcd65cd1f43defc4ff64c268
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-21 16:44:18 +0200

    add Category.php

--- Models/Foods.php
+++ Models/Foods.php
@@ -1,6 +1,6 @@
 <?php
 include_once __DIR__ . '/Product.php';
-// include_once __DIR__ . '/Product.php';
+include_once __DIR__ . '/Category.php';
 class Foods extends Product
 {
     public string $type;
